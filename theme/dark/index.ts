8170bf593c6f15f0cddcf43079689ce843708797
melisa-abuin <melisa.abuin@gmail.com>
2024-09-08 17:46:21 +0200

    feature #17: create themes

---
+++ theme/dark/index.ts
@@ -0,0 +1,20 @@
+const darkTheme = {
+  name: 'dark',
+  color: {
+    primary: '#B24141',
+    secondary: '#41B2B2',
+    tertiary: '',
+  },
+  background: {
+    primary: '',
+    secondary: '',
+    tertiary: '',
+  },
+  text: {
+    primary: '',
+    secondary: '',
+    tertiary: '',
+  },
+}
+
+export default darkTheme
