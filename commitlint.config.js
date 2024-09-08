f2a2e1682bae67f173e3706f921289690237763b
melisa-abuin <melisa.abuin@gmail.com>
2024-09-08 13:50:42 +0200

    mnasjdsa

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,11 +9,6 @@ module.exports = {
     'subject-case': [0],
     'subject-empty': [0],
     'type-empty': [0],
-    'header-pattern': [
-      2,
-      'always',
-      /^(feature|fix|refactor|deps|docs|test)[\s]+#[0-9]+:[\s]+.+$/,
-    ], // Remove this in case we want to use the standard commit structure <type>(<optional scope>): <subject>
     'header-max-length': [0],
   },
 }
