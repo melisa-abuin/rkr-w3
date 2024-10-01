daafce6fbe388e25acb93f65f4ecd64ca4cee921
melisa-abuin <melisa.abuin@gmail.com>
2024-10-02 00:08:58 +0200

    feature #8: add tricks array data

--- constants/index.ts
+++ constants/index.ts
@@ -25,3 +25,36 @@ export const routes = {
     url: '#',
   },
 }
+
+export const tricks = [
+  {
+    alt: 'Lane trick button',
+    icon: '/camera-angle.png',
+    text: 'In the 7th and 11th lane you can pass through the upper torch, this may serve you to avoid dogs or take a shortcut to the next lane. (Waiting for confirmation)',
+    videoUrl: '/camera-angle.mp4',
+  },
+  {
+    alt: 'Dog trick button',
+    icon: '/paw-heart.png',
+    text: 'If someone saves you and you are inside a dog, you can move through him without dying. This way you can overcome dog walls or take a path blocked for the one who saved you.',
+    videoUrl: '/camera-angle.mp4',
+  },
+  {
+    alt: 'Group trick button',
+    icon: '/two-users.png',
+    text: 'In order to control two different kitties at the same time you must set up 3 control groups. Select your Kitty and press crtl+1, select the other Kitty and press ctrl+2 and now select your Kitty and press shift while you select the other kitty to select both kitties at the same time and finally press ctrl+3 with both kitties selected.',
+    videoUrl: '/camera-angle.mp4',
+  },
+  {
+    alt: 'Camera angle trick button',
+    icon: '/camera-angle.png',
+    text: 'Find out what keys change the camera angle, this is useful for hard saves with almost no visibility.',
+    videoUrl: '/camera-angle.mp4',
+  },
+  {
+    alt: 'Center camera trick button',
+    icon: '/camera-center.png',
+    text: 'Press F1 to center the camera in your Kitty.',
+    videoUrl: '/camera-angle.mp4',
+  },
+]
