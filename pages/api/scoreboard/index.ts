60b45dbe92435bf820ddb229448e982148fd6863
melisa-abuin <melisa.abuin@gmail.com>
2024-10-24 22:24:45 +0200

    feature #7: format api data

---
+++ pages/api/scoreboard/index.ts
@@ -0,0 +1,44 @@
+import { mockApiData } from '@/constants'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+type ObjectKey = Record<string, string | number>
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY
+
+  try {
+    if (!apiKey) {
+      throw new Error()
+    }
+    // replace with api call
+    /*const response = await fetch(apiKey, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()*/
+
+    const data = mockApiData
+
+    // if the data volume increases here we will need to implement a cache/invalidation method
+    const formattedData = data.map((elem) => {
+      const newObject: ObjectKey = {}
+
+      Object.entries(elem).map(([key, value]) => {
+        console.log(key, value)
+        const newKey = key.toLowerCase().replace(/ : /g, ' ').replace(/ /g, '_')
+        newObject[newKey] = value
+      })
+      return newObject
+    })
+
+    res.status(200).json(formattedData)
+  } catch (error) {
+    console.error('Error fetching scoreboard data:', error)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
