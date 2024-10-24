1138dbe51263849ca9922452fee9bcdb2f3c478b
melisa-abuin <melisa.abuin@gmail.com>
2024-10-24 22:25:13 +0200

    feature #7: add dummy data

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,16 +1,11 @@
 'use client'
 
 import Table from '@/components/table'
-const data = [
-  { name: 'Player1', saves: 10, deaths: 2 },
-  { name: 'Player2', saves: 5, deaths: 6 },
-  { name: 'Player3', saves: 7, deaths: 3 },
-]
+import { useFetch } from '@/hooks/useFetch'
+import { PlayersStats } from '@/interfaces/player'
 
 export default function Leaderboard() {
-  return (
-    <main>
-      <Table data={data} />
-    </main>
-  )
+  const { data, loading, error } = useFetch<PlayersStats>('/api/scoreboard')
+
+  return <main>{!loading && !error && <Table data={data} />}</main>
 }
--- components/table/index.tsx
+++ components/table/index.tsx
@@ -7,32 +7,28 @@ import {
   Container,
   Title,
 } from './styled'
-
-interface PlayerStats {
-  name: string
-  saves: number
-  deaths: number
-}
+import { PlayersStats } from '@/interfaces/player'
 
 interface TableProps {
-  data: PlayerStats[]
+  data: PlayersStats | null
 }
-export default function Table({ data }: TableProps) {
+
+export default function Table(data: TableProps) {
   return (
     <Container aria-labelledby="scoreboard-title">
       <StyledTable role="table" aria-label="Player Stats">
         <Title id="scoreboard-title">Scoreboard</Title>
         <thead>
           <StyledTr>
-            <StyledTh scope="col">Name</StyledTh>
+            <StyledTh scope="col">Player</StyledTh>
             <StyledTh scope="col">Saves</StyledTh>
             <StyledTh scope="col">Deaths</StyledTh>
           </StyledTr>
         </thead>
         <tbody>
-          {data.map((player, index) => (
+          {data?.data?.map((player, index) => (
             <tr key={index}>
-              <StyledTd data-label="Name">{player.name}</StyledTd>
+              <StyledTd data-label="Player">{player.name}</StyledTd>
               <StyledTd data-label="Saves">{player.saves}</StyledTd>
               <StyledTd data-label="Deaths">{player.deaths}</StyledTd>
             </tr>
