c1f6483a54d6e6e5cd34fd936341ca0490d42c92
melisa-abuin <melisa.abuin@gmail.com>
2024-11-23 14:20:57 +0100

    docs #69: document utils functions

--- utils/calculateSaveDeathRatio/index.ts
+++ utils/calculateSaveDeathRatio/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Provides the save/death ratio for a player
+ * if the player has no saves then it returns 0, if the player has no deaths it returns the saves instead
+ *
+ * @param saves amount of saves
+ * @param deaths amount of deaths
+ * @returns save/death ratio
+ */
 export const calculateSaveDeathRatio = (saves: number, deaths: number) => {
   if (!saves) {
     return 0
