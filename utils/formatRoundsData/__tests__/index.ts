03eb5f654c01816232802315b6a535dd65d8f671
melisa-abuin <melisa.abuin@gmail.com>
2024-11-26 20:24:41 +0100

    test #72: fix test type

--- utils/formatRoundsData/__tests__/index.ts
+++ utils/formatRoundsData/__tests__/index.ts
@@ -1,15 +1,19 @@
-import { formattedMockData } from '@/constants'
 import { formatRoundsData } from '..'
 
 describe('formatRoundsData', () => {
   it('Returns a new object with normal, hard, impossible and a total time calculated', () => {
-    expect(formatRoundsData(formattedMockData[0], 1)).toStrictEqual({
-      normal: formattedMockData[0].round1TimeNormal,
-      hard: formattedMockData[0].round1TimeHard,
-      impossible: formattedMockData[0].round1TimeImpossible,
+    const mockedData = {
+      round1TimeNormal: 90.2,
+      round1TimeHard: 105.2,
+      round1TimeImpossible: 150.5,
+    }
+    expect(formatRoundsData(mockedData, 1)).toStrictEqual({
+      normal: mockedData.round1TimeNormal,
+      hard: mockedData.round1TimeHard,
+      impossible: mockedData.round1TimeImpossible,
       best: {
         difficulty: 'normal',
-        time: formattedMockData[0].round1TimeNormal,
+        time: mockedData.round1TimeNormal,
       },
     })
   })
