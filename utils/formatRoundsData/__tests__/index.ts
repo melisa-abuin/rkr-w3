221ec8c6e73c9c15de9355b0c8a4458a88e88515
melisa-abuin <melisa.abuin@gmail.com>
2024-11-26 18:52:33 +0100

    test #72: change tests for format rounds data

--- utils/formatRoundsData/__tests__/index.ts
+++ utils/formatRoundsData/__tests__/index.ts
@@ -5,8 +5,8 @@ describe('formatRoundsData', () => {
   it('Returns a new object with normal, hard, impossible and a total time calculated', () => {
     expect(formatRoundsData(formattedMockData[0], 1)).toStrictEqual({
       normal: formattedMockData[0].round1TimeNormal,
-      hard: formattedMockData[0].round_1_time_hard,
-      impossible: formattedMockData[0].round_1_time_impossible,
+      hard: formattedMockData[0].round1TimeHard,
+      impossible: formattedMockData[0].round1TimeImpossible,
       best: {
         difficulty: 'normal',
         time: formattedMockData[0].round1TimeNormal,
