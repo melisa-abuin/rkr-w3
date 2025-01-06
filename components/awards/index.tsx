749334807588c007878aabbc38aec6c565a7fef6
melisa-abuin <melisa.abuin@gmail.com>
2025-01-06 21:54:18 +0100

    feature #104: create section for rewards

--- components/awards/index.tsx
+++ components/awards/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Award } from '@/interfaces/player'
-import { Body, CircleImage, Header } from './styled'
+import { Body, CircleImage, Header, Section, Title } from './styled'
 
 interface Props {
   awards: Award[]
@@ -11,16 +11,21 @@ export default function Awards({ awards }: Props) {
     <div>
       <Header>Game Awards</Header>
       <Body>
-        {awards.map(({ id, completed }) => (
-          <CircleImage
-            alt="ewe"
-            key={id}
-            height={48}
-            completed={completed}
-            src="/image2.png"
-            width={48}
-          />
-        ))}
+        <div>
+          <Title>Wings</Title>
+          <Section>
+            {awards.map(({ id, completed }) => (
+              <CircleImage
+                alt="ewe"
+                key={id}
+                height={48}
+                completed={completed}
+                src="/image2.png"
+                width={48}
+              />
+            ))}
+          </Section>
+        </div>
       </Body>
     </div>
   )
