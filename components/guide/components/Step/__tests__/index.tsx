b469708d1a2d8de9a832990b52fb8702cb0e5dd9
melisa-abuin <melisa.abuin@gmail.com>
2025-02-04 20:08:07 +0100

    fix #108: fix next image test

--- components/guide/components/Step/__tests__/index.tsx
+++ components/guide/components/Step/__tests__/index.tsx
@@ -2,6 +2,20 @@ import { screen } from '@testing-library/react'
 import Step from '..'
 import { renderWithTheme } from '@/utils/renderWithTheme'
 
+interface ImageProps {
+  src: string
+  alt: string
+  onError: () => void
+}
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: jest.fn(({ src, alt, onError }: ImageProps) => (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img src={src} alt={alt} onError={onError} />
+  )),
+}))
+
 describe('Step', () => {
   it('renders step text correctly', () => {
     renderWithTheme(<Step topText="top text" bottomText="bottom text" />)
