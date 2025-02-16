78407592ab11308436f61cbfd824e0cdf992dd32
melisa-abuin <melisa.abuin@gmail.com>
2025-02-16 01:10:18 +0100

    feature #137: update link component to have link styles

--- components/link/index.tsx
+++ components/link/index.tsx
@@ -4,10 +4,13 @@ import React, { ReactNode } from 'react'
 import { StyledLink } from './styled'
 
 interface Props {
+  withButtonStyle?: boolean
   href: string
   children: ReactNode
 }
 
-export const Link = ({ href, children }: Props) => (
-  <StyledLink href={href}>{children}</StyledLink>
+export const Link = ({ withButtonStyle = false, children, href }: Props) => (
+  <StyledLink withButtonStyle={withButtonStyle} href={href}>
+    {children}
+  </StyledLink>
 )
