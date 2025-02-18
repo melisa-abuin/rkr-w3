814e21b414f26fa149169cf0430d8bbb66b95cc2
melisa-abuin <melisa.abuin@gmail.com>
2025-02-18 21:06:21 +0100

    fix #137: fix case path

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/footer'
-import Step from '@/components/step'
+import Step from '@/components/step2'
 import Navbar from '@/components/navbar'
 import { PageContainer } from '@/components/pageContainer'
 import PageHeader from '@/components/pageHeader'
