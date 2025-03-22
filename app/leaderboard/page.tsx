f68f366a6ed21aa2c04fc9ad2b3074e4cdd1116a
melisa-abuin <melisa.abuin@gmail.com>
2025-03-22 11:05:29 +0100

    feature #174: create leaderboard template

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,20 +1,10 @@
 import Error from '@/components/molecules/error'
-import Table from '@/components/molecules/table'
-import { BestTime, PlayersStats } from '@/interfaces/player'
+import { BestTime } from '@/interfaces/player'
 import { ThemeProvider } from '@/hooks/useTheme'
 import Navbar from '@/components/molecules/navbar'
 import Footer from '@/components/molecules/footer'
-import PageHeader from '@/components/atoms/pageHeader'
-import { statsColumns } from '@/constants'
 import { headers } from 'next/headers'
-import ColumnCards from '@/components/molecules/columnCards'
-import { PageContainer } from '@/components/atoms/pageContainer'
-import Link from '@/components/atoms/link'
-import ColumnCardsWithControls from '@/components/organisms/columnCardsWithControls'
-import Info from '@/components/atoms/info'
-import PlayerFinderWithResult from '@/components/organisms/playerFinderWithResult'
-import { GamesStats } from '@/interfaces/game'
-import BestGamesWithControls from '@/components/organisms/bestGamesWithControls'
+import LeaderboardTemplate from '@/components/templates/leaderboard'
 
 interface Data {
   player: string
@@ -24,17 +14,9 @@ interface Data {
 interface PlayerStatsData {
   error: string | null
   data: {
-    scoreboard?: PlayersStats
-    leaderboard?: {
-      stats: Array<{ category: string; key: string; data: Data[] }>
-      times: Array<{ category: string; key: string; data: Data[] }>
-    }
+    stats: Array<{ category: string; key: string; data: Data[] }>
+    times: Array<{ category: string; key: string; data: Data[] }>
   }
-}
-
-interface GameStatsData {
-  error: string | null
-  data: GamesStats
 }
 
 async function fetchData(): Promise<PlayerStatsData> {
@@ -43,7 +25,7 @@ async function fetchData(): Promise<PlayerStatsData> {
   const host = headersList.get('host')
   const url = `${protocol}://${host}`
 
-  const response = await fetch(`${url}/api/scoreboard`)
+  const response = await fetch(`${url}/api/leaderboard`)
   if (response.status === 200) {
     return {
       data: await response.json(),
@@ -51,115 +33,19 @@ async function fetchData(): Promise<PlayerStatsData> {
     }
   }
   return {
-    data: {},
-    error: 'Something went wrong',
-  }
-}
-
-async function fetchGameTimes(): Promise<GameStatsData> {
-  const headersList = headers()
-  const protocol = headersList.get('x-forwarded-proto') || 'http'
-  const host = headersList.get('host')
-  const url = `${protocol}://${host}`
-
-  const response = await fetch(`${url}/api/gameTimes`)
-  if (response.status === 200) {
-    return {
-      data: await response.json(),
-      error: null,
-    }
-  }
-  return {
-    data: [],
+    data: { stats: [], times: [] },
     error: 'Something went wrong',
   }
 }
 
 export default async function Leaderboard() {
   const { data, error } = await fetchData()
-  const { data: gameData, error: gameError } = await fetchGameTimes()
 
   return (
     <ThemeProvider>
       <Navbar />
       <main>
-        {error || gameError ? (
-          <Error />
-        ) : (
-          <>
-            <PageHeader
-              description="Best times and scores of Run Kitty Run players. The scores shown on this page are subject to the files uploaded by the players, if a player is not present in this table it is because they have not uploaded their statistics in the latest versions of the game"
-              title="Leaderboard"
-            />
-            <PageContainer>
-              <PlayerFinderWithResult />
-            </PageContainer>
-            <PageContainer
-              ariaLabelledby="columns-score-title"
-              title="Best scores"
-            >
-              <ColumnCards
-                data={data.leaderboard?.stats}
-                viewAllKey="overview"
-              />
-            </PageContainer>
-
-            <ColumnCardsWithControls
-              data={data.leaderboard?.times}
-              viewAllKey="time"
-              title="Best times"
-            />
-            <PageContainer
-              ariaLabelledby="columns-best-games-title"
-              title="Best game times"
-              marginTop={32}
-            >
-              <BestGamesWithControls data={gameData} />
-            </PageContainer>
-            <Table
-              columns={statsColumns}
-              data={data.scoreboard}
-              title="Leaderboard Highlights: Top Five Stats"
-            />
-            <PageContainer as="div" marginBottom={48}>
-              <Link
-                colorName="tertiary"
-                href="/stats/overview?page=1&sortKey=completedChallenges&sortOrder=desc"
-                withButtonStyle
-              >
-                View all stats
-              </Link>
-            </PageContainer>
-            <Info as="ul">
-              <li>
-                <strong>Can&apos;t find your stats?</strong>{' '}
-                <Link
-                  colorName="tertiary"
-                  href="/guide"
-                  aria-label="Go to guide"
-                >
-                  Click here
-                </Link>{' '}
-                to learn how to upload your game progress and get featured on
-                the leaderboard!
-              </li>
-              <li>
-                Don&apos;t want your BattleTag displayed on this page? Join our{' '}
-                <Link
-                  colorName="tertiary"
-                  href="https://discord.com/channels/873715731873804298/1344673409468207186"
-                  aria-label="Discord server"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Discord server
-                </Link>{' '}
-                and contact a member with the &quot;Developer&quot; tag to
-                request it to be anonymized.
-              </li>
-            </Info>
-          </>
-        )}
+        {error ? <Error /> : data && <LeaderboardTemplate data={data} />}
       </main>
       <Footer />
     </ThemeProvider>
