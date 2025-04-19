3c37b6f6e9ac96b8f738c5e276086fe38e30d0c3
melisa-abuin <melisa.abuin@gmail.com>
2025-04-19 14:07:16 +0200

    feature #246: add kibble collected leaderboard

---
+++ pages/api/kibbleStats/index.ts
@@ -0,0 +1,59 @@
+import { ApiPlayerStats, PlayerStats } from '@/interfaces/player'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { fetchData } from '@/utils/fetchData'
+
+interface QueryParams {
+  difficulty?: 'normal' | 'hard' | 'impossible' | undefined
+}
+
+type StatsRequest = NextApiRequest & { query: QueryParams }
+
+export default async function handler(req: StatsRequest, res: NextApiResponse) {
+  try {
+    const data = await fetchData('players')
+
+    const formattedData = data
+      .map((elem: ApiPlayerStats) => {
+        const saveData = JSON.parse(elem['Save Data'])
+
+        const { PlayerName, KibbleCurrency, PersonalBests } = saveData
+        const playerStats: Partial<PlayerStats> = {}
+
+        playerStats.battleTag = {
+          name: PlayerName?.split('#')[0] || '',
+          tag: PlayerName || '',
+        }
+
+        if (!KibbleCurrency || !PersonalBests) {
+          playerStats.kibbles = {
+            collectedAllTime: 0,
+            jackpots: 0,
+            superJackpots: 0,
+            collectedSingleGame: 0,
+          }
+        } else {
+          playerStats.kibbles = {
+            collectedAllTime: KibbleCurrency?.Collected,
+            jackpots: KibbleCurrency?.Jackpots,
+            superJackpots: KibbleCurrency.SuperJackpots,
+            collectedSingleGame: PersonalBests.KibbleCollected,
+          }
+        }
+
+        return playerStats as PlayerStats
+      })
+      .sort(
+        (a: PlayerStats, b: PlayerStats) =>
+          b.kibbles.collectedSingleGame - a.kibbles.collectedSingleGame,
+      )
+      .slice(0, 5)
+
+    console.log(formattedData)
+    const stats = formattedData
+
+    res.status(200).json(stats)
+  } catch (error) {
+    console.error('Error fetching scoreboard data:', error)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
