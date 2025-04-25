1eb3959b504c54468ac73f6b9748e6dd8c39a385
melisa-abuin <melisa.abuin@gmail.com>
2025-04-25 12:41:19 +0200

    refactor #244: improve floating kibble logic

--- components/organisms/kibbleLeaderboardWithMoreResults/components/floatingKibble/index.tsx
+++ components/organisms/kibbleLeaderboardWithMoreResults/components/floatingKibble/index.tsx
@@ -7,20 +7,20 @@ import Lottie from 'lottie-react'
 import animationData from '@/public/animations/spark.json'
 import { getKibbleRewardMessage } from '@/utils/getKibbleRewardMessage'
 
+type AnimationState = 'idle' | 'spark' | 'done'
+
 export default function FloatingKibble() {
-  const [showKibble, setShowKibble] = useState(true)
-  const [showSpark, setShowSpark] = useState(false)
+  const [animationState, setAnimationState] = useState<AnimationState>('idle')
   const [rewardMessage, setRewardMessage] = useState('')
 
   const handleClick = () => {
     setRewardMessage(getKibbleRewardMessage())
-    setShowKibble(false)
-    setShowSpark(true)
+    setAnimationState('spark')
   }
 
   return (
-    <Container collapsed={!showKibble && !showSpark}>
-      {showKibble && (
+    <Container collapsed={animationState === 'done'}>
+      {animationState === 'idle' && (
         <>
           <ImageWrapper onClick={handleClick}>
             <Image alt="kibble" height={48} width={48} src="/kibble.png" />
@@ -28,13 +28,13 @@ export default function FloatingKibble() {
           <Text>Pick it up!</Text>
         </>
       )}
-      {!showKibble && showSpark && (
+      {animationState === 'spark' && (
         <>
           <Lottie
             animationData={animationData}
             loop={false}
             autoplay
-            onComplete={() => setShowSpark(false)}
+            onComplete={() => setAnimationState('done')}
             style={{ width: 64, height: 64 }}
           />
           <Text>{rewardMessage}</Text>
