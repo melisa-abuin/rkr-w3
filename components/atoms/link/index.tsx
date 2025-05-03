13646a2f6c244896a15cabf2d5198426ec576341
melisa-abuin <melisa.abuin@gmail.com>
2025-05-03 14:28:18 +0200

    feature #221: refactor link component to match new theme structure

--- components/atoms/link/index.tsx
+++ components/atoms/link/index.tsx
@@ -6,37 +6,43 @@ import { useTheme } from '@/hooks/useTheme'
 
 interface Props {
   children: ReactNode
-  colorName?: string
+  color?: string
   download?: string
   href: string
   onClick?: () => void
   rel?: string
   target?: '_self' | '_blank'
-  withButtonStyle?: boolean
 }
 
 export default function Link({
   children,
-  colorName,
+  color = 'primary',
   download,
   href,
   onClick,
   rel,
   target = '_self',
-  withButtonStyle = false,
 }: Props) {
   const [theme] = useTheme()
-  const color = colorName ? theme.text[colorName] : theme.text.primary
+  const colorName = theme.text.color[color]
+  const hoverColor = theme.text.hover[color]
+
+  if (!colorName || !hoverColor) {
+    console.error(
+      `Text color "${color}" is not defined in ${theme.name} theme.`,
+    )
+    return null
+  }
 
   return (
     <StyledLink
-      color={color || theme.text.primary}
+      color={colorName}
+      hoverColor={hoverColor}
       download={download}
       href={href}
       onClick={onClick}
       rel={rel}
       target={target}
-      withButtonStyle={withButtonStyle}
     >
       {children}
     </StyledLink>
