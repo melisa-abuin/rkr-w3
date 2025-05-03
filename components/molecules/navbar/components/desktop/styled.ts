37862ed5a7338d08a63b2c22f71399a16f51c791
melisa-abuin <melisa.abuin@gmail.com>
2025-05-03 19:01:06 +0200

    feature #221: change link style on navbar

--- components/molecules/navbar/components/desktop/styled.ts
+++ components/molecules/navbar/components/desktop/styled.ts
@@ -5,6 +5,10 @@ interface NavLinkProps {
 }
 
 export const NavLink = styled.li<NavLinkProps>`
+  position: relative;
+  padding: 16px 16px;
+  margin: 0 2px;
+
   a {
     font-size: var(--font-size-xs-sm);
     font-weight: var(
@@ -13,9 +17,25 @@ export const NavLink = styled.li<NavLinkProps>`
     );
     text-decoration: none;
   }
-  padding: 16px 18px;
-  border-bottom: ${({ selected, theme }) =>
-    selected ? `2px solid ${theme.text.primary}` : 'none'};
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 2px;
+    background-color: ${({ theme }) => theme.text.color.primary};
+    transform: scaleX(${({ selected }) => (selected ? 1 : 0)});
+    transform-origin: center;
+    transition: transform 0.3s ease;
+  }
+
+  &:hover::after {
+    background-color: ${({ theme }) => theme.text.hover.tertiary};
+
+    transform: scaleX(1);
+  }
 
   @media (max-width: 700px) {
     display: none;
