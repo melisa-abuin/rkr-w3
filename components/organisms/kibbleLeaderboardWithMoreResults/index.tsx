95893009680796bb5a228bf6b20018bdb7a63474
melisa-abuin <melisa.abuin@gmail.com>
2025-05-26 21:48:24 +0200

    feat #188: react query for kibble leaderboard

--- components/organisms/kibbleLeaderboardWithMoreResults/index.tsx
+++ components/organisms/kibbleLeaderboardWithMoreResults/index.tsx
@@ -1,66 +1,42 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import { kibbleColumns } from '@/constants'
-import { useToast } from '@/hooks/useToast'
 import Table from '@/components/molecules/table'
 import RowCards from '@/components/molecules/rowCards'
 import { DetailedPlayerStats } from '@/interfaces/player'
 import FloatingKibble from './components/floatingKibble'
+import { useApiQuery } from '@/hooks/useApiQuery'
+import { useQueryErrorToast } from '@/hooks/useQueryErrorToast'
 
 export default function KibbleLeaderboardWithMoreResults() {
-  const [filteredData, setFilteredData] = useState<
-    DetailedPlayerStats[] | undefined
-  >()
-  const [loading, setLoading] = useState(true)
-  const { showToast } = useToast()
+  const { data, isFetching, error } = useApiQuery<DetailedPlayerStats[]>(
+    '/api/kibbleLeaderboard',
+    undefined,
+    {
+      enabled: true,
+    },
+  )
 
-  useEffect(() => {
-    const fetchFilteredData = async () => {
-      setLoading(true)
-
-      // TODO: create helper or what about react query?
-      try {
-        const response = await fetch('/api/kibbleLeaderboard', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`)
-        }
-
-        const result = await response.json()
-        setFilteredData(result)
-      } catch (error) {
-        showToast(
-          `Couldn't load the kibbles leaderboard, please try again later.`,
-        )
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchFilteredData()
-  }, [showToast])
+  useQueryErrorToast(
+    error,
+    `Couldn't load the kibbles leaderboard, please try again later.`,
+  )
 
   return (
     <>
       <FloatingKibble />
-      <RowCards data={filteredData?.slice(0, 5)} loading={loading} />
+      <RowCards data={data?.slice(0, 5)} loading={isFetching} />
       <Table
         columns={kibbleColumns}
         data={
-          filteredData
+          data
             ?.map((elem) => ({
               battleTag: elem.battleTag,
               ...elem.kibbles,
             }))
             .slice(5, 20) || []
         }
-        loading={loading}
+        loading={isFetching}
       />
     </>
   )
