c57b3d420ddba8b363601137083a424f385535ca
CongestriAriel <congestriariel@gmail.com>
2025-06-01 22:50:36 +0200

    scripts: Fix relative path and regex improvements

--- scripts/atomicStructureCheck.py
+++ scripts/atomicStructureCheck.py
@@ -23,6 +23,7 @@ def find_forbidden_imports(root_folder, patterns):
         for file in files:
             if file.endswith(".tsx"):
                 file_path = os.path.join(folder_path, file)
+                print(file_path)
                 forbidden_lines = find_forbidden_lines(file_path, patterns)
                 if forbidden_lines:
                     violations[file_path] = forbidden_lines
@@ -32,37 +33,37 @@ def find_forbidden_imports(root_folder, patterns):
 def scan_atoms():
     """Set rules for files inside atoms folder and returns a dictionary with errors"""
     forbidden_patterns = [
-        r"import\s+\w+\s+from\s+['\"]@/components/atoms/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/molecules/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/organisms/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/templates/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]\.\./[^'\"]+['\"]" 
+        r"import\s+.*\s+from\s+['\"]@/components/atoms/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/molecules/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/organisms/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/templates/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]\.\./[^'\"]+['\"]" 
     ]
-    atoms_path = os.path.abspath("./components/atoms")
+    atoms_path = os.path.abspath("../components/atoms")
     return find_forbidden_imports(atoms_path, forbidden_patterns)
 
 def scan_molecules():
     """Set rules for files inside molecules folder and returns a dictionary with errors"""
     forbidden_patterns = [
-        r"import\s+\w+\s+from\s+['\"]@/components/molecules/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/organisms/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/templates/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/[^'\"]+/[^'\"]+/components/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]\.\./\.\./atoms/[^'\"]+['\"]"
+        r"import\s+.*\s+from\s+['\"]@/components/molecules/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/organisms/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/templates/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/[^'\"]+/[^'\"]+/components/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]\.\./\.\./atoms/[^'\"]+['\"]"
     ]
-    atoms_path = os.path.abspath("./components/molecules")
+    atoms_path = os.path.abspath("../components/molecules")
     return find_forbidden_imports(atoms_path, forbidden_patterns)
 
 def scan_organisms():
     """Set rules for files inside organisims folder and returns a dictionary with errors"""
     forbidden_patterns = [
-        r"import\s+\w+\s+from\s+['\"]@/components/organisms/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/templates/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]@/components/[^'\"]+/[^'\"]+/components/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]\.\./\.\./molecules/[^'\"]+['\"]",
-        r"import\s+\w+\s+from\s+['\"]\.\./\.\./atoms/[^'\"]+['\"]"
+        r"import\s+.*\s+from\s+['\"]@/components/organisms/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/templates/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]@/components/[^'\"]+/[^'\"]+/components/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]\.\./\.\./molecules/[^'\"]+['\"]",
+        r"import\s+.*\s+from\s+['\"]\.\./\.\./atoms/[^'\"]+['\"]"
     ]
-    atoms_path = os.path.abspath("./components/organisms")
+    atoms_path = os.path.abspath("../components/organisms")
     return find_forbidden_imports(atoms_path, forbidden_patterns)
 
 def report_violations(violations, message):
@@ -75,12 +76,11 @@ def report_violations(violations, message):
 
 def main():
     print("Scanning...")
-
     atoms_violations = scan_atoms()
     molecules_violations = scan_molecules()
     organisms_violations = scan_organisms()
     if not atoms_violations and not molecules_violations and not organisms_violations:
-      print("\nNo forbidden patterns found.")
+      print("No forbidden patterns found.")
       sys.exit(0)
 
     report_violations(atoms_violations, "\nAtoms with forbidden imports found:")
