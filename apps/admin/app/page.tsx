3ae481e03a4b8b6f5e4f09b6f0574040af1ca8d3
melisa-abuin <melisa.abuin@gmail.com>
2026-08-05 21:32:01 +0200

    fix #415: fix css

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,7 +1,15 @@
+import PageContainer from '@rkr/dls/components/atoms/pageContainer'
+import PageHeader from '@rkr/dls/components/atoms/pageHeader'
+
 export default function AdminPage() {
   return (
     <main>
-      <h1>Admin</h1>
+      <PageContainer>
+        <PageHeader
+          description="Set up and manage the RKR website, including banners, events, and more."
+          title="RKR Admin"
+        />
+      </PageContainer>
     </main>
   )
 }
